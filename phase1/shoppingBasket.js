eecc0ed35a684079b885be5f7b23f41ebd098374
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-07-27 15:33:06 +0100

    completed classes challenge

---
+++ phase1/shoppingBasket.js
@@ -0,0 +1,25 @@
+const Candy = require('./candy')
+
+class ShoppingBasket {
+  constructor(){
+    this.allCandies= []
+  }
+  addItem(candy){
+    this.allCandies = this.allCandies.concat(candy)
+  }
+  getTotalPrice(){
+    let sum = 0
+    this.allCandies.map((candy) =>{
+      sum += candy.getPrice()
+    });
+    return sum
+  }
+
+}
+
+basket = new ShoppingBasket();
+console.log(basket.addItem(new Candy("Skittle", 3.99)));
+console.log(basket.addItem(new Candy("Skittle", 2.99)));
+console.log(basket.getTotalPrice());
+
+module.exports = ShoppingBasket;
